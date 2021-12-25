c01fce5f5cf46dd8569ea1425fbcde16df6b808a
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2021-12-24 16:51:22 -0800

    Add files via upload
    added the 3 requirements

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            < iframe width = "560" height = "315" src = "https://www.youtube.com/embed/eRBOgtp0Hac?start=20" title = "YouTube video player" frameborder = "0" allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></ iframe >;
+        }
+    }
+}
